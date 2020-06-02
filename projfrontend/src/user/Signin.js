ed09acf4ae6ee478af50b71faf87c3bf1eed7c45
Ayush8605 <ayush.shar05@gmail.com>
2020-06-02 15:19:56 -0400

    added signin functionality

--- projfrontend/src/user/Signin.js
+++ projfrontend/src/user/Signin.js
@@ -1,31 +1,125 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
 import Base from "../core/Base";
 
-const SigninForm = () => {
-  return (
-    <div className="row">
-      <div className="col-md-6 offset-sm-3 text-left">
-        <form>
-          <div className="form-group">
-            <label className="text-light">Email</label>
-            <input type="email" className="form-control"></input>
-          </div>
-          <div className="form-group">
-            <label className="text-light">Password</label>
-            <input type="password" className="form-control"></input>
-          </div>
-          <button className="btn btn-success btn-block">Submit</button>
-        </form>
-      </div>
-    </div>
-  );
-};
+import {
+  signin,
+  authenticate,
+  isAuthenticated,
+  signup,
+} from "../auth/helper/index.js";
 
 const Signin = () => {
+  const [values, setValues] = useState({
+    email: "tulip@gmail.com",
+    password: "123456",
+    error: "",
+    loading: false,
+    didRedirect: false,
+  });
+
+  const { email, password, error, loading, didRedirect } = values;
+  const { user } = isAuthenticated();
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false, loading: true });
+    signin({ email, password })
+      .then((data) => {
+        if (data.error) {
+          setValues({ ...values, error: data.error, loading: false });
+        } else {
+          authenticate(data, () => {
+            setValues({
+              ...values,
+              didRedirect: true,
+            });
+          });
+        }
+      })
+      .catch(console.log("sign in failed"));
+  };
+
+  const performRedirect = () => {
+    if (didRedirect) {
+      if (user && user.role === 1) {
+        return <p>redirect to admin</p>;
+      } else {
+        return <p>redirect to user dashboard</p>;
+      }
+    }
+    if (isAuthenticated()) {
+      return <Redirect to="/" />;
+    }
+  };
+
+  const loadingMessage = () => {
+    return (
+      loading && (
+        <div className="alert alert-info">
+          <h2>Loading...</h2>
+        </div>
+      )
+    );
+  };
+
+  const errorMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-danger"
+            style={{ display: error ? "" : "none" }}
+          >
+            {error}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const SigninForm = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-light">Email</label>
+              <input
+                type="email"
+                onChange={handleChange("email")}
+                value={email}
+                className="form-control"
+              ></input>
+            </div>
+            <div className="form-group">
+              <label className="text-light">Password</label>
+              <input
+                type="password"
+                onChange={handleChange("password")}
+                value={password}
+                className="form-control"
+              ></input>
+            </div>
+            <button onClick={onSubmit} className="btn btn-success btn-block">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  };
   return (
     <Base title="Sign In" description="Sign in to enter the epic merch store">
+      {loadingMessage()}
+      {errorMessage()}
       {SigninForm()}
+      {performRedirect()}
+      <p className="text-white text-center">{JSON.stringify(values)}</p>
     </Base>
   );
 };
