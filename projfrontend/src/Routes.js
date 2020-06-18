dfce1c951503aa282c28b1611e5df6b3695bd827
Ayush8605 <ayush.shar05@gmail.com>
2020-06-17 20:48:35 -0400

    manage product added

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -7,6 +7,11 @@ import AdminRoute from "./auth/helper/AdminRoutes";
 import PrivateRoute from "./auth/helper/PrivateRoutes";
 import UserDashBoard from "./user/UserDashBoard";
 import AdminDashBoard from "./user/AdminDashBoard";
+import AddCategory from "./admin/AddCategory";
+import ManageCategories from "./admin/ManageCategories";
+import AddProduct from "./admin/AddProduct";
+import ManageProducts from "./admin/ManageProducts";
+import UpdateProduct from "./admin/UpdateProduct";
 
 const Routes = () => {
   return (
@@ -25,6 +30,31 @@ const Routes = () => {
             path="/admin/dashboard"
             exact
             component={AdminDashBoard}
+          ></AdminRoute>
+          <AdminRoute
+            path="/admin/create/category"
+            exact
+            component={AddCategory}
+          ></AdminRoute>
+          <AdminRoute
+            path="/admin/categories"
+            exact
+            component={ManageCategories}
+          ></AdminRoute>
+          <AdminRoute
+            path="/admin/create/product"
+            exact
+            component={AddProduct}
+          ></AdminRoute>
+          <AdminRoute
+            path="/admin/products"
+            exact
+            component={ManageProducts}
+          ></AdminRoute>
+          <AdminRoute
+            path="/admin/product/update/:productId"
+            exact
+            component={UpdateProduct}
           ></AdminRoute>
         </Switch>
       </BrowserRouter>
