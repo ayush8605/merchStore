9a216bd07ebda4221d495ed5e8a16e89b6071096
Ayush8605 <ayush.shar05@gmail.com>
2020-06-26 19:39:11 -0400

    added product display as card on home

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles.css";
 import { API } from "../backend";
 import Base from "./Base";
+import Card from "./card";
+import { getProducts } from "./helper/coreapicalls";
 
 export default function Home() {
-  console.log("API IS", process.env.REACT_APP_BACKEND);
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(false);
+
+  const loadAllProducts = () => {
+    getProducts().then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProducts(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadAllProducts();
+  }, []);
+
   return (
-    <Base title="Home">
-      <h1 className="text-white">hello this is the home screen {API}</h1>
+    <Base title="Home Page" description="Welcome to the Tshirt Store">
+      <div className="row text-center">
+        <h1 className="text-white">All Tshirts</h1>
+        <div className="row">
+          {products.map((product, index) => {
+            return (
+              <div key={index} className="col-4 mb-4">
+                <Card product={product} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </Base>
   );
 }
